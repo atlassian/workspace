7fe77b1b796582f224d496a239d9b26307af2404
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2018-09-03 17:44:04 +0200

    Stabilize dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,17 @@ plugins {
     id("com.atlassian.performance.tools.gradle-release").version("0.4.0")
 }
 
+configurations.all {
+    resolutionStrategy {
+        failOnVersionConflict()
+        eachDependency {
+            when (requested.module.toString()) {
+                "org.slf4j:slf4j-api" -> useVersion("1.8.0-alpha2")
+            }
+        }
+    }
+}
+
 dependencies {
     listOf(
         "org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion",
